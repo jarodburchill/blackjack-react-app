1f90b1864034c9532f774f4b88717026bb066693
Jarod Burchill <jarodburchill@gmail.com>
2019-08-19 15:18:15 -0400

    Created JSON deck

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 
 const App: React.FC = () => {
+  const deck = (require('../deck.json')).cards;
+  const activeCard = deck[Math.floor(Math.random() * 52)];
+  let suitSymbol = '';
+  switch (activeCard.suit) {
+    case 'spades':
+      suitSymbol = '♠'
+      break;
+    case 'diamonds':
+      suitSymbol = '♦'
+      break;
+    case 'clubs':
+      suitSymbol = '♣'
+      break;
+    case 'hearts':
+      suitSymbol = '♥'
+      break;
+    default:
+      break;
+  }
   return (
     <div>
+      <h1>{activeCard.value}</h1>
+      <h1>{suitSymbol}</h1>
     </div>
   );
 }
