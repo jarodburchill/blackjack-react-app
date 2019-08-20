bcd12544499250f17919fa8af6594708b75d26f7
Jarod Burchill <jarodburchill@gmail.com>
2019-08-20 09:01:48 -0400

    Button to draw from deck

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App: React.FC = () => {
   const deck = (require('../deck.json')).cards;
-  const randomIndex = Math.floor(Math.random() * 52);
-  const activeCard = deck[randomIndex];
-  deck.splice(randomIndex, 1);
-  console.log(deck);
-  let suitSymbol = '';
-  switch (activeCard.suit) {
-    case 'spades':
-      suitSymbol = '♠'
-      break;
-    case 'diamonds':
-      suitSymbol = '♦'
-      break;
-    case 'clubs':
-      suitSymbol = '♣'
-      break;
-    case 'hearts':
-      suitSymbol = '♥'
-      break;
-    default:
-      break;
+  const [value, setValue] = useState("");
+  const [suit, setSuit] = useState("");
+
+  const drawCard = () => {
+    const randomIndex = Math.floor(Math.random() * deck.length);
+    const activeCard = deck[randomIndex];
+    deck.splice(randomIndex, 1);
+    console.log(deck);
+    console.log(randomIndex);
+    console.log(activeCard);
+    setValue(activeCard.value);
+    switch (activeCard.suit) {
+      case 'spades':
+        setSuit('♠');
+        break;
+      case 'diamonds':
+        setSuit('♦');
+        break;
+      case 'clubs':
+        setSuit('♣');
+        break;
+      case 'hearts':
+        setSuit('♥');
+        break;
+      default:
+        break;
+    }
   }
+
+  const clickHandler = (e: any) => {
+    e.preventDefault()
+    drawCard();
+  }
+
+
   return (
-    <div>
-      <h1>{activeCard.value}</h1>
-      <h1>{suitSymbol}</h1>
-    </div>
+    <>
+      <div>
+        <h1>{value}</h1>
+        <h1>{suit}</h1>
+      </div>
+      <button onClick={clickHandler}>Test</button>
+    </>
   );
 }
 
