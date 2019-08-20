9ea8fc0b456ade70c1d877e5ff74a4ba6d3416ba
Jarod Burchill <jarodburchill@gmail.com>
2019-08-20 11:40:48 -0400

    Draws multiple cards

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,46 +1,58 @@
 import React, { useState } from 'react';
+import styles from './styles/App.module.css';
 import Card from './Card';
 
 const App: React.FC = () => {
   const deck = (require('../deck.json')).cards;
-  const [value, setValue] = useState("");
-  const [suit, setSuit] = useState("");
+  const [playerCards, setPlayerCards]: any[] = useState([]);
+
+  // const playersCards = [{ 'value': '7', 'suit': '♠' }, { 'value': '8', 'suit': '♠' }];
 
   const drawCard = () => {
     const randomIndex = Math.floor(Math.random() * deck.length);
     const activeCard = deck[randomIndex];
     deck.splice(randomIndex, 1);
-    console.log(deck);
-    console.log(randomIndex);
-    console.log(activeCard);
-    setValue(activeCard.value);
+    console.log('Random Index:', randomIndex);
+    console.log('Last Pulled Card:', activeCard);
+    console.log('Remaining Cards:', deck);
+    console.log('Player Cards Array 0:', playerCards);
     switch (activeCard.suit) {
       case 'spades':
-        setSuit('♠');
+        setPlayerCards([...playerCards, { 'value': activeCard.value, 'suit': '♠' }]);
         break;
       case 'diamonds':
-        setSuit('♦');
+        setPlayerCards([...playerCards, { 'value': activeCard.value, 'suit': '♦' }]);
         break;
       case 'clubs':
-        setSuit('♣');
+        setPlayerCards([...playerCards, { 'value': activeCard.value, 'suit': '♣' }]);
         break;
       case 'hearts':
-        setSuit('♥');
+        setPlayerCards([...playerCards, { 'value': activeCard.value, 'suit': '♥' }]);
         break;
       default:
         break;
     }
+    console.log('Player Cards Array 1:', playerCards);
   }
 
   const clickHandler = () => {
     drawCard();
+    console.log('Player Cards Array 2:', playerCards);
   }
 
+  //setPlayerCards(playerCards.push({ 'value': '8', 'suit': '♥' }));
+  console.log('Player Cards Array 3:', playerCards);
 
   return (
     <>
-      <button onClick={clickHandler}>Test</button>
-      <Card value={value} suit={suit} />
+      <button onClick={clickHandler}>Draw a Card</button>
+      <div className={styles.container}>
+        {playerCards.map((card: any, index: number) => {
+          return (
+            <Card key={index} value={card.value} suit={card.suit} />
+          );
+        })}
+      </div>
     </>
   );
 }
