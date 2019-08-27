19393de771f6a1453bd48561491321ef3c0d6dab
Jarod Burchill <jarodburchill@gmail.com>
2019-08-27 09:35:11 -0400

    Betting validation

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles/Controls.module.css';
 
 type ControlsProps = {
@@ -13,9 +13,33 @@ type ControlsProps = {
 
 const Controls: React.FC<ControlsProps> = ({ balance, gameState, buttonState, betEvent, hitEvent, standEvent, resetEvent }) => {
   const [amount, setAmount] = useState(10);
+  const [inputStyle, setInputStyle] = useState(styles.input);
+
+  useEffect(() => {
+    validation();
+  }, [amount]);
+
+  const validation = () => {
+    if (amount > balance) {
+      setInputStyle(styles.inputError);
+      return false;
+    }
+    if (amount <= 0) {
+      setInputStyle(styles.inputError);
+      return false;
+    }
+    setInputStyle(styles.input);
+    return true;
+  }
 
   const amountChange = (e: any) => {
     setAmount(e.target.value);
+  }
+
+  const onBetClick = () => {
+    if (validation()) {
+      betEvent(amount);
+    }
   }
 
   const getControls = () => {
@@ -24,9 +48,9 @@ const Controls: React.FC<ControlsProps> = ({ balance, gameState, buttonState, be
         <div className={styles.controlsContainer}>
           <div className={styles.betContainer}>
             <h4>Amount:</h4>
-            <input type='number' min='10' max={balance} step='10' value={amount} onChange={amountChange} className={styles.input} />
+            <input autoFocus type='number' value={amount} onChange={amountChange} className={inputStyle} />
           </div>
-          <button onClick={() => betEvent(amount)} className={styles.button}>Bet</button>
+          <button onClick={() => onBetClick()} className={styles.button}>Bet</button>
         </div>
       );
     }
